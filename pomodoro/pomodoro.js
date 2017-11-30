2deaebf5e028fd3231aaf7a43461c70752a005e5
alpera <33016508+alper4@users.noreply.github.com>
2017-11-30 14:18:44 +0300

    use button instead of :button

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -1,49 +1,49 @@
 (function() {
     var tmMin, tmSec, timerOn=false, timerPaused=false, tmId, totSec, elapsed, work;
-    $(":button:eq(0)").click(function() {
+    $("button:eq(0)").click(function() {
         if (timerPaused)
             timerOn = timerPaused = false;
-        let v = Number($(":button:eq(1)").text());
+        let v = Number($("button:eq(1)").text());
         if (v < 90) /* max 90 min work */
-            $(":button:eq(1)").text(v+1);
+            $("button:eq(1)").text(v+1);
     });
-    $(":button:eq(2)").click(function() {
+    $("button:eq(2)").click(function() {
         if (timerPaused)
             timerOn = timerPaused = false;
-        let v = Number($(":button:eq(1)").text());
+        let v = Number($("button:eq(1)").text());
         if (v > 1)
-            $(":button:eq(1)").text(v-1);
+            $("button:eq(1)").text(v-1);
     });
-    $(":button:eq(3)").click(function() {
+    $("button:eq(3)").click(function() {
         if (timerPaused)
             timerOn = timerPaused = false;
-        let v = Number($(":button:eq(4)").text());
+        let v = Number($("button:eq(4)").text());
         if (v < 60) /* max 60 min break */
-            $(":button:eq(4)").text(v+1);
+            $("button:eq(4)").text(v+1);
     });
-    $(":button:eq(5)").click(function() {
+    $("button:eq(5)").click(function() {
         if (timerPaused)
             timerOn = timerPaused = false;
-        let v = Number($(":button:eq(4)").text());
+        let v = Number($("button:eq(4)").text());
         if (v > 1)
-            $(":button:eq(4)").text(v-1);
+            $("button:eq(4)").text(v-1);
     });
     
-    $(":button:eq(1)").click(function() {
+    $("button:eq(1)").click(function() {
         if (timerPaused) {
             tmId = window.setInterval(cbTmr, 1000);
             $(".progress-bar").addClass("active");
-            $(":button:eq(0), :button:eq(2)").prop("disabled", true);
+            $("button:eq(0), button:eq(2)").prop("disabled", true);
             timerPaused = false;
         }
         else if (timerOn) {
             window.clearInterval(tmId);
             $(".progress-bar").removeClass("active");
-            $(":button:eq(0), :button:eq(2)").prop("disabled", false);
+            $("button:eq(0), button:eq(2)").prop("disabled", false);
             timerPaused = true;
         }
         else {
-            tmMin = Number($(":button:eq(1)").text());
+            tmMin = Number($("button:eq(1)").text());
             tmSec = 0;
             totSec = tmMin * 60;
             elapsed = 0;
@@ -53,27 +53,27 @@
             tmId = window.setInterval(cbTmr, 1000);
             timerOn = true;
             work = true;
-            $(":button:eq(4)").prop("disabled", true);
-            $(":button:eq(3), :button:eq(5)").prop("disabled", false);
-            $(":button:eq(0), :button:eq(2)").prop("disabled", true);
+            $("button:eq(4)").prop("disabled", true);
+            $("button:eq(3), button:eq(5)").prop("disabled", false);
+            $("button:eq(0), button:eq(2)").prop("disabled", true);
         }
     });
             
-    $(":button:eq(4)").click(function() {
+    $("button:eq(4)").click(function() {
         if (timerPaused) {
             tmId = window.setInterval(cbTmr, 1000);
             $(".progress-bar").addClass("active");
-            $(":button:eq(3), :button:eq(5)").prop("disabled", true);
+            $("button:eq(3), button:eq(5)").prop("disabled", true);
             timerPaused = false;
         }
         else if (timerOn) {
             window.clearInterval(tmId);
             $(".progress-bar").removeClass("active");
-            $(":button:eq(3), :button:eq(5)").prop("disabled", false);
+            $("button:eq(3), button:eq(5)").prop("disabled", false);
             timerPaused = true;
         }
         else {
-            tmMin = Number($(":button:eq(4)").text());
+            tmMin = Number($("button:eq(4)").text());
             tmSec = 0;
             totSec = tmMin * 60;
             elapsed = 0;
@@ -82,9 +82,9 @@
             tmId = window.setInterval(cbTmr, 1000);
             timerOn = true;
             work = false;
-            $(":button:eq(1)").prop("disabled", true);
-            $(":button:eq(0), :button:eq(2)").prop("disabled", false);
-            $(":button:eq(3), :button:eq(5)").prop("disabled", true);
+            $("button:eq(1)").prop("disabled", true);
+            $("button:eq(0), button:eq(2)").prop("disabled", false);
+            $("button:eq(3), button:eq(5)").prop("disabled", true);
         }
     });
     
@@ -95,11 +95,11 @@
                 $(".progress-bar").removeClass("active");
                 timerOn = false;
                 if (work) {
-                    $(":button:eq(4)").prop("disabled", false).trigger("click");
+                    $("button:eq(4)").prop("disabled", false).trigger("click");
                     work = false;
                 }
                 else {
-                    $(":button:eq(1)").prop("disabled", false).trigger("click");
+                    $("button:eq(1)").prop("disabled", false).trigger("click");
                     work = true;
                 }
                 return;
