4307ad73e126f93e03aa5051a9728af35b2d5a25
alper4 <33016508+alper4@users.noreply.github.com>
2017-12-08 00:38:21 +0300

    satisfy jshint

--- simon/simon.js
+++ simon/simon.js
@@ -1,3 +1,4 @@
+/* jshint esversion: 6 */
 (function() {
     const dur = 500, off_dur = 200;
     const maxit = 19;
@@ -14,36 +15,39 @@
         window.setTimeout(() => roboPlay(tur), dur);
     });
     
-    for (let i = 0; i < 4; ++i)
-        $("button").eq(i).click(function() {
-            if (inplay) return;
-            if (i != seq[tur]) {
-                $("audio source[src^='fail']").parent().eq(0)[0].play();
-                if ($("input:checkbox").is(":checked"))
-                    window.setTimeout(() => $("button.btn-default").trigger("click"), dur*2);
-                else {
-                    tur = 0;
-                    window.setTimeout(() => roboPlay(tur), dur*2);
-                }
-            }
+    let btnclk = function(e) {
+        if (inplay) return;
+        let i = e.data;
+        if (i != seq[tur]) {
+            $("audio source[src^='fail']").parent().eq(0)[0].play();
+            if ($("input:checkbox").is(":checked"))
+                window.setTimeout(() => $("button.btn-default").trigger("click"), dur*2);
             else {
-                inplay = true;
-                b[i].play();
-                if (tur == itera)
-                    if (itera == maxit)
-                        window.setTimeout(() => gSuccess(), dur*2);
-                    else {
-                        ++itera;
-                        tur = 0;
-                        window.setTimeout(() => {
-                            $(".cntr").text(Number($(".cntr").text()) + 1);
-                            roboPlay(tur);
-                        }, dur*2);
-                    }
-                else
-                    window.setTimeout(() => { ++tur; inplay = false; }, dur);
+                tur = 0;
+                window.setTimeout(() => roboPlay(tur), dur*2);
             }
-        });
+        }
+        else {
+            inplay = true;
+            b[i].play();
+            if (tur == itera)
+                if (itera == maxit)
+                    window.setTimeout(() => gSuccess(), dur*2);
+                else {
+                    ++itera;
+                    tur = 0;
+                    window.setTimeout(() => {
+                        $(".cntr").text(Number($(".cntr").text()) + 1);
+                        roboPlay(tur);
+                    }, dur*2);
+                }
+            else
+                window.setTimeout(() => { ++tur; inplay = false; }, dur);
+        }
+    };
+    
+    for (let i = 0; i < 4; ++i)
+        $("button").eq(i).click(i, btnclk);
     
     function roboPlay(i) {
         $("button").eq(seq[i]).addClass("active focus");
